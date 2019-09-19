36dadcc31b4bab3fd90eb6772bf133d4fd9e9fd7
Ashkan Yousefi <ashkan.yousefi@gmail.com>
2019-09-19 19:34:32 +1000

    HW2 updated.

--- HW1/Q1/script.py
+++ HW1/Q1/script.py
@@ -6,30 +6,31 @@ import timeit
 import sys
 import collections
 from pygexf.gexf import *
-
 from pprint import pprint
 import urllib.request # I have added as it is a standard library_AY.
+from tqdm.auto import tqdm
 
 #%%
-# ash_key=str(sys.argv)
-
-
-#%%
-key = '50ea93987447c4d7483b02d78f98f2ce'
-# key = sys.argv[1]
-
+command_line=sys.argv[1]
+key=str(command_line)
 
 #%%
+def do_api(url_address):
+	data=urllib.request.urlopen(url_address)
+	response=data.read()
+	encoding=data.info().get_content_charset('utf-8')
+	raw_data=json.loads(response.decode(encoding))
+	return raw_data
+
+#%%
+
 def min_parts():
 	"""
 	Returns an integer value
 	"""
 	# you must replace this with your own value
-	return 270
+	return 1150
 
-#%%
-
-# ? There is a message that urllib does not have a request module. However, it works just fine in the 
 # Jupyter notebook running on Anaconda
 def lego_sets():
 	"""
@@ -43,61 +44,46 @@ def lego_sets():
 	e.g., len(my_sets)
 	"""
 	# you must replace this line and return your own list
-	max_parts = 300
-	page_size = 10
-	limit = 300
+	page_size = 10000
 
 	url='https://rebrickable.com/api/v3/lego/sets/?'\
-    'page_size={}&ordering=-num_parts&key={}&limit={}'\
-    .format(page_size,key,limit)
+    'page_size={}&ordering=-num_parts&key={}&min_parts={}'\
+    .format(page_size,key,min_parts())
 
-	# url='https://rebrickable.com/api/v3/lego/sets/?'\
-	# 	'page_size={}&min_parts={}&max_parts={}&key={}&oredering=num_parts'\
-	# 	.format(page_size, min_parts(), max_parts, key)
-	data=urllib.request.urlopen(url)
+	raw_data = do_api(url)
 	
-	response=data.read()
-	encoding=data.info().get_content_charset('utf-8')
-	raw_data=json.loads(response.decode(encoding))
-	
-	raw_data['results'] = raw_data['results'][:300]
-	# total_set_num=[]
-	# for i in range(300):
-	# 	total_set_num.append(raw_data['results'][i]['set_num'])
-	return raw_data['results']
+	print(raw_data['count'])
+	raw_data = raw_data['results'][:300]
+	return raw_data
 
-results = lego_sets()
+all_sets = lego_sets()
 
 #%%
-set_number_parts={}
 
-for set_num in results[0:3]:
-
-	# ?? Not sure why do I need to close the broken line with ' 
-	url='https://rebrickable.com/api/v3/lego/sets/parts/?'\
-		'key={}/'.format(key)
-
-	data=urllib.request.urlopen(url)
-	# temp_read_data=data.read()
-	# encoding=data.info().get_content_charset('utf-8')
-	# raw_data_part=json.loads(temp_read_data.decode(encoding))
-
-
-	# print(raw_data_part)
-	# part_color=raw_data_part['results']
-	# part_quantity
-	# part_name
-	# part_number
-	
-	# set_number_parts.update()
-
-#%%
-#This is a test cell 
-f=open('Results_Dictionary.txt','r')
-data=f.read()
+all_parts = []
+for lego_set in tqdm(all_sets[:10]):
+	url='https://rebrickable.com/api/v3/lego/sets/{}/parts/?key={}'\
+			.format(lego_set['set_num'], key)
+	raw_data = do_api(url)
+	raw_data = raw_data['results'][:20]
+	for part in raw_data:
+		all_parts.append(
+			{
+				'id': part['part']['part_num'] + '_' + part['color']['rgb'],
+				'color': part['color']['rgb'],
+				'quantity': part['quantity'],
+				'name': part['part']['name'],
+				'number': part['part']['part_num'],
+				'set_num': lego_set['set_num']
+			}
+		)
 
 
 #%%
+
+def hex2rgb(hex):
+	return tuple(str(int(hex[i:i+2], 16)) for i in (0, 2, 4))
+
 #Constructing a graph using the pygexf
 def gexf_graph():
 	"""
@@ -107,21 +93,34 @@ def gexf_graph():
 	from gexf import Gexf, GexfImport
 
 	# test helloworld.gexf
-	gexf = Gexf("Paul Girard","A hello world! file")
-	graph=gexf.addGraph("directed","static","a hello world graph")
+	gexf = Gexf("Ashkan Yousefi", "A hello world! file")
+	graph = gexf.addGraph("undirected", "static", "a hello world graph")
 
-	graph.addNode("0","hello")
-	graph.addNode("1","World")
-	graph.addEdge("0","0","1")
+	for lego_set in all_sets:
+		node = graph.addNode(id=lego_set['set_num'], label=lego_set['name'],
+							 r='0', g='0', b='0')
+		node.Type = 'set'
+	
+	for lego_part in all_parts:
+		rgb_color = hex2rgb(lego_part['color'])
+		node = graph.addNode(id=lego_part['id'], label=lego_part['name'],
+							 r=rgb_color[0], g=rgb_color[1], b=rgb_color[2])
+		node.Type = 'part'
 
-	output_file=open("helloworld.gexf","w")
-	gexf.write(output_file)
+	for i, lego_part in enumerate(all_parts):
+		node = graph.addEdge(id=str(i), source=lego_part['set_num'], target=lego_part['id'], weight=lego_part['quantity'])
+		node.Type = 'part'
+
+	with open("lego_graph.gexf","wb") as lego_file:
+		gexf.write(lego_file)
 
 	# you must replace these lines and supply your own graph
 	# my_gexf = Gexf("author", "title")
 	# gexf.addGraph("undirected", "static", "I'm an empty graph")
 	# return gexf.graphs[0]
+gexf_graph()
 
+#%%
 # complete auto-grader functions for Q1.2.d
 
 def avg_node_degree():
@@ -149,20 +148,3 @@ def avg_path_length():
 	"""
 	# you must replace this value with the avg path length
 	return -1
-#%%
-def get_lego_parts(leg_set):
-	set_numbers = [lego_set['set_num'] for lego_set in results['results']]
-	page_size = 1000
-	for lego_sets in set_numbers:
-		ulr = 'https://rebrickable.com/api/v3/lego/sets/{}/parts/?key={}&page_size={}'.format(lego_sets, key, page_size)
-		data=urllib.request.urlopen(url)
-		response=data.read()
-		encoding=data.info().get_content_charset('utf-8')
-		raw_data=json.loads(response.decode(encoding))
-
-# for each in range(len(Set_Num)):
-#     Temp=API_URL_Template.split('/')
-#     Temp[7]=Set_Num[each]
-#     API_URL='/'.join(Temp)
-#     API_Response=requests.get(API_URL,{'key':value_key})
-#     API_Data.update([(Set_Num[each],API_Response.json())])
